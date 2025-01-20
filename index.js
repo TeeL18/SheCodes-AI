b65e7e05e29f7a8f8d4dfea35c0a28d93f7ab025
TeeL18 <torrence_95@hotmail.com>
2025-01-20 20:16:17 +0000

    A little challenge I was testing myself on

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-function showAnswer(response) {
-  alert(response.data.answer);
+let challengeOne = document.querySelector("#one");
+challengeOne.setAttribute("disabled", "disabled");
+
+function handleClick() {
+  let challengeTwo = document.querySelector("#two");
+  challengeTwo.style.backgroundColor = "green";
 }
 
-let apiKey = "oe47bb3454cd0f1af7dfdf430teb7c08";
-let context =
-  "provide a precise and short simple answer. Please make sure just to pick one answer;";
-let prompt = "who was the first femalew president?";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+let buttonElement = document.querySelector("#two");
+buttonElement.addEventListener("click", handleClick);
 
-axios.get(apiUrl).then(showAnswer);
+function pressClick() {
+  let challengeThree = document.querySelector("#three");
+  challengeThree.classList.add("danger");
+}
+
+let buttonThreeElement = document.querySelector("#three");
+buttonThreeElement.addEventListener("click", pressClick);
