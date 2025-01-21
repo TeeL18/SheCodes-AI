72a59a0304a98e279f0f00c03e64705980737d49
TeeL18 <torrence_95@hotmail.com>
2025-01-21 18:26:05 +0000

    playing around with typewriter plugin

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-let challengeOne = document.querySelector("#one");
-challengeOne.setAttribute("disabled", "disabled");
+let heading = document.querySelector("h1");
+heading.innerHTML = "Tee Acheam";
 
-function handleClick() {
-  let challengeTwo = document.querySelector("#two");
-  challengeTwo.style.backgroundColor = "green";
-}
-
-let buttonElement = document.querySelector("#two");
-buttonElement.addEventListener("click", handleClick);
-
-function pressClick() {
-  let challengeThree = document.querySelector("#three");
-  challengeThree.classList.add("danger");
-}
-
-let buttonThreeElement = document.querySelector("#three");
-buttonThreeElement.addEventListener("click", pressClick);
+new Typewriter("h2", {
+  strings: "Tee Acheam!💅🏾",
+  autoStart: true,
+  cursor: "",
+});
